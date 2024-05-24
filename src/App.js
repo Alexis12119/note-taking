e0c9ba3d36fef66daa56cc7fe745fd8afece5d5f
Alexis12119 <corporal461@gmail.com>
2024-05-24 10:32:38 +0800

    feat: change name

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-semibold mb-4">My Notes</h1>
+      <h1 className="text-3xl font-semibold mb-4">Take Notes</h1>
 
       <input
         type="text"
