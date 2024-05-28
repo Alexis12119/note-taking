fb536f55aa464a35aeb099ab630da00dbac4bab6
Alexis12119 <corporal461@gmail.com>
2024-05-29 07:19:42 +0800

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { initializeApp } from "firebase/app";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import {
   getFirestore,
   collection,
@@ -14,20 +18,26 @@ import {
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZ5RK_BVc7nIYKbpugMb-xJ059cffFLp0",
-
   authDomain: "notes-eb8df.firebaseapp.com",
-
   projectId: "notes-eb8df",
-
   storageBucket: "notes-eb8df.appspot.com",
-
   messagingSenderId: "573088404098",
-
   appId: "1:573088404098:web:0a1cc74b13b1a90aa91a9b",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+const toolbarOptions = [
+  [{ header: "1" }, { header: "2" }, { font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline", "strike", "blockquote"],
+  [{ script: "sub" }, { script: "super" }],
+  [{ color: [] }, { background: [] }],
+  [{ align: [] }],
+  ["link", "image", "video"],
+  ["clean"],
+];
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -38,9 +48,7 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
   const [noteToDelete, setNoteToDelete] = useState(null);
-  const [theme, setTheme] = useState(
-    () => localStorage.getItem("theme") || "light",
-  );
+  const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "light");
 
   useEffect(() => {
     document.documentElement.className = theme;
@@ -48,7 +56,6 @@ function App() {
     fetchNotes();
   }, [theme]);
 
-  // Fetch notes function
   async function fetchNotes() {
     try {
       const snapshot = await getDocs(collection(db, "notes"));
@@ -65,21 +72,23 @@ function App() {
   async function addNote() {
     await addDoc(collection(db, "notes"), { title, content });
     setShowAddNoteModal(false);
+    fetchNotes();
   }
 
   async function saveNoteChanges() {
     await updateDoc(doc(db, "notes", selectedNote.id), { title, content });
     setSelectedNote(null);
     setShowAddNoteModal(false);
+    fetchNotes();
   }
 
   async function deleteNote() {
     await deleteDoc(doc(db, "notes", noteToDelete.id));
     setShowDeleteConfirmation(false);
+    fetchNotes();
   }
 
   useEffect(() => {
-    // Listen for changes in the notes collection
     const unsubscribe = onSnapshot(collection(db, "notes"), (snapshot) => {
       const notesData = snapshot.docs.map((doc) => ({
         id: doc.id,
@@ -88,7 +97,6 @@ function App() {
       setNotes(notesData);
     });
 
-    // Clean up the listener
     return () => {
       unsubscribe();
     };
@@ -98,21 +106,22 @@ function App() {
     setShowDeleteConfirmation(true);
     setNoteToDelete(note);
   }
+
   function handleAddNoteButtonClick() {
-    setSelectedNote(null); // Reset selected note
-    setTitle(""); // Reset title
-    setContent(""); // Reset content
+    setSelectedNote(null);
+    setTitle("");
+    setContent("");
     setShowAddNoteModal(true);
   }
 
   function handleNoteClick(note, event) {
-    // Check if the clicked element is the delete button
-    if (event.target.classList.contains("delete-button")) {
+    if (event.target.classList.contains("delete-icon")) {
       return;
     }
     setSelectedNote(note);
     setTitle(note.title);
     setContent(note.content);
+    setShowAddNoteModal(true);
   }
 
   function handleSearchChange(e) {
@@ -122,7 +131,7 @@ function App() {
   const filteredNotes = notes.filter(
     (note) =>
       note.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      note.content.toLowerCase().includes(searchQuery.toLowerCase()),
+      note.content.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
@@ -132,7 +141,7 @@ function App() {
         <button
           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
           className="text-xl"
-          style={{ border: "none" }} // Add this inline style
+          style={{ border: "none" }}
         >
           {theme === "light" ? "🌙" : "☀️"}
         </button>
@@ -148,9 +157,7 @@ function App() {
 
       {filteredNotes.length === 0 && (
         <div className="flex justify-center items-center h-full">
-          <p className="text-gray-500 text-center text-2xl font-bold">
-            No notes found.
-          </p>
+          <p className="text-gray-500 text-center text-2xl font-bold">No notes found.</p>
         </div>
       )}
 
@@ -161,25 +168,27 @@ function App() {
             className="card cursor-pointer relative overflow-hidden"
             onClick={(event) => handleNoteClick(note, event)}
           >
+            <div className="absolute top-2 right-2">
+              <FontAwesomeIcon
+                icon={faTimes}
+                className="delete-icon text-red-500 hover:text-red-600 cursor-pointer"
+                onClick={() => confirmDeleteNote(note)}
+              />
+            </div>
             <div>
               <h2 className="text-xl font-semibold">{note.title}</h2>
-              <p className="text-gray-700 truncate text-lg">{note.content}</p>
-              <br />
-              <br />
-            </div>
-            <div className="absolute bottom-4 right-4">
-              <button
-                className="bg-red-500 hover:bg-red-600 text-white font-semibold px-3 py-2 rounded-md delete-button"
-                onClick={() => confirmDeleteNote(note)}
-              >
-                Delete
-              </button>
+              <div
+                className={`text-gray-700 truncate text-lg ${
+                  theme === "dark" && "text-white"
+                }`}
+                dangerouslySetInnerHTML={{ __html: note.content }}
+              ></div>
             </div>
           </div>
         ))}
       </div>
 
-      {selectedNote && (
+      {(selectedNote || showAddNoteModal) && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
           <div
             className="bg-white p-8 rounded-md shadow-lg max-w-3xl relative border border-gray-300"
@@ -191,7 +200,9 @@ function App() {
               overflow: "auto",
             }}
           >
-            <h2 className="text-xl font-semibold mb-4">Edit Note</h2>
+            <h2 className="text-xl font-semibold mb-4">
+              {selectedNote ? "Edit Note" : "Add Note"}
+            </h2>
             <input
               type="text"
               placeholder="Title"
@@ -199,64 +210,38 @@ function App() {
               value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
-            <textarea
-              placeholder="Content"
-              className="w-full h-40 mb-2 p-2 border-2 border-solid rounded-md resize-none"
+            <ReactQuill
               value={content}
-              onChange={(e) => setContent(e.target.value)}
+              onChange={setContent}
+              className="tp mb-2"
+              theme="snow"
+              modules={{ toolbar: toolbarOptions }}
+              style={theme === "dark" ? { color: "#fff" } : {}}
             />
             <button
               className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md mr-2"
-              onClick={saveNoteChanges}
+              onClick={selectedNote ? saveNoteChanges : addNote}
             >
               Save
             </button>
             <button
               className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold px-4 py-2 rounded-md"
-              onClick={() => setSelectedNote(null)}
+              onClick={() => {
+                setSelectedNote(null);
+                setShowAddNoteModal(false);
+              }}
             >
               Cancel
             </button>
             <button
               className="absolute top-0 right-0 mt-4 mr-4 bg-transparent border-none cursor-pointer"
-              onClick={() => setSelectedNote(null)}
-            ></button>
-          </div>
-        </div>
-      )}
-
-      {showAddNoteModal && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-8 rounded-md shadow-lg max-w-lg border border-gray-300">
-            <h2 className="text-xl font-semibold mb-4">Add Note</h2>
-            <input
-              type="text"
-              placeholder="Title"
-              className="w-full mb-2 p-2 border border-gray-300 rounded-md"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-
-            <textarea
-              placeholder="Content"
-              className="w-full h-40 mb-2 p-2 border border-gray-300 rounded-md resize-none"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-            />
-            <div className="flex justify-end">
-              <button
-                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md mr-2"
-                onClick={addNote}
-              >
-                Save
-              </button>
-              <button
-                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold px-4 py-2 rounded-md"
-                onClick={() => setShowAddNoteModal(false)}
-              >
-                Cancel
-              </button>
-            </div>
+              onClick={() => {
+                setSelectedNote(null);
+                setShowAddNoteModal(false);
+              }}
+            >
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
           </div>
         </div>
       )}
